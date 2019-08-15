15ef2b830e30b8f1ba918b82111478a8ae9a2944
sergey <sergvcx@mail.ru>
2019-08-15 13:52:19 +0300

    login ++

--- tinder-unhider.py
+++ tinder-unhider.py
@@ -8,45 +8,84 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options 
  
- 
-driver = webdriver.Firefox()
+#driver = webdriver.Firefox()
 #driver = webdriver.Chrome()
 #driver.implicitly_wait(10) # seconds
 
 
 
+
+
+#chrome_options = Options()
+#chrome_options.add_argument("--user-data-dir=chrome-data")
+#driver = webdriver.Chrome(options=chrome_options)
+#chrome_options.add_argument("user-data-dir=chrome-data") 
+chrome_options = Options()
+chrome_options.add_argument("--user-data-dir=chrome-data")
+driver = webdriver.Chrome('chromedriver.exe',options=chrome_options)
+#chrome_options.add_argument("user-data-dir=chrome-data") 
 driver.get("https://tinder.com/")
+
+#time.sleep(160)  # Time to enter credentials
+
+
+
+
+
+#print("load cookies")
+#cookies = pickle.load(open("tinder2.pkl","rb"))
+#for cookie in cookies:
+#    driver.add_cookie(cookie)
+#
+
+#driver.refresh()	
 #time.sleep(5)
-element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div[2]/div/button/span/span")))
-#results = driver.find_elements_by_class_name('button__text Pos(r) Z(1) D(ib)')
+#element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div/div/div[2]/div/div[3]/div[1]/button/span")))
+print("======== log in ================================")
+#element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, "//span[@class='button__text Pos(r) Z(1)' and contains(text(), 'Log in')]")))
+element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, "//span[@class='button__text Pos(r) Z(1)']")))
+
+time.sleep(5)
+driver.refresh();
+#exit();
+time.sleep(5)
+
+#element = WebDriverWait(driver, 5).until(EC.text_to_be_present_in_element("Log in"))
+#element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'button__text.Pos(r).Z(1).D(ib)')))
 #element = driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div[2]/div/button/span/span')
-print(element)
+print("text=",element.text)
+print("class=",element.get_attribute("class"))
+element.click();
+
+print("====== login by phone number ==================================")
+#element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div/div/div[2]/div/div[3]/div[1]/button/span/span")))
+#element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, "//span[@class='button__text Pos(r) Z(1) D(ib)' and contains(text(), 'Log in with phone number')]")))
+element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, "//span[@class='button__text Pos(r) Z(1) D(ib)']")))
 print(element.text)
 element.click();
 
-element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div/div/div[2]/div/div[3]/div[1]/button/span/span")))
-#element = driver.find_element_by_xpath('/html/body/div[2]/div/div/div[2]/div/div[3]/div[1]/button/span/span')
-print(element)
-print(element.text)
-element.click();
 
+print("======= enter phone number =================================")
 #time.sleep(5)
 element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div/div/div[2]/div[2]/div/input")))
 #element = driver.find_element_by_xpath('/html/body/div[2]/div/div/div[2]/div[2]/div/input')
-print(element)
 print(element.text)
-#element.send_keys("")
-#element.send_keys(Keys.RETURN)
-
+element.send_keys("9265330272")
+element.send_keys(Keys.RETURN)
 time.sleep(30)
 
-all_cookies = driver.get_cookies()
-print("cookies=",all_cookies)
+print("======= save coockies =================================")
+#all_cookies = driver.get_cookies()
+#pickle.dump(all_cookies, open("tinder2.pkl","wb"))
+#print("cookies=",all_cookies)
 
+
+print("======= open hidden pics =================================")
 #element = WebDriverWait(driver, 200).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div/div[1]/div/aside/nav/div/div/div/div[2]/div[1]/div[1]/div[1]/a/div/div[2]/div")))
-element = WebDriverWait(driver, 200).until(EC.presence_of_element_located((By.XPATH, " /html/body/div[1]/div/div[1]/div/aside/nav/div/div/div/div[2]/div[1]/div[1]/div[1]/a/div/div[2]")))
-#element = WebDriverWait(driver, 200).until(EC.presence_of_element_located((By.CLASS_NAME, "StretchedBox.CenterAlign")))
+#element = WebDriverWait(driver, 200).until(EC.presence_of_element_located((By.XPATH, " /html/body/div[1]/div/div[1]/div/aside/nav/div/div/div/div[2]/div[1]/div[1]/div[1]/a/div/div[2]")))
+element = WebDriverWait(driver, 200).until(EC.presence_of_element_located((By.CLASS_NAME, "StretchedBox.CenterAlign")))
 
 #element = driver.find_element_by_xpath('/html/body/div[2]/div/div/div[2]/div[2]/div/input')
 print(element)
@@ -61,6 +100,10 @@ print(element)
 print(element.text)
 elements = driver.find_elements_by_class_name('Expand.enterAnimationContainer')
 i=0
+
+
+
+
 for e in elements:
 	#print("e e=",e)
 	#print("e tag=",e.tag_name)
