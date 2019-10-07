7e994b38766c5b11adbb792088732dc48d04d3c9
sergey <sergvcx@mail.ru>
2019-10-07 11:30:50 +0300

    go

---
+++ tinder.go
@@ -0,0 +1,27 @@
+What do you think about Валерия?
+
+https://go.tinder.com/dJ6zl12a9sc-%D0%92%D0%B0%D0%BB%D0%B5%D1%80%D0%B8%D1%8FWhat do you think about Анюта?
+
+https://go.tinder.com/Xcibq12a9tc-%D0%90%D0%BD%D1%8E%D1%82%D0%B0What do you think about Валерия?
+
+https://go.tinder.com/LwY3gF2a9uo-%D0%92%D0%B0%D0%BB%D0%B5%D1%80%D0%B8%D1%8FWhat do you think about Лера?
+
+https://go.tinder.com/C29cCV2a9vo-%D0%9B%D0%B5%D1%80%D0%B0What do you think about Виктория?
+
+https://go.tinder.com/U2CP3F2a9ww-%D0%92%D0%B8%D0%BA%D1%82%D0%BE%D1%80%D0%B8%D1%8FWhat do you think about Olesya?
+
+https://go.tinder.com/g5Cjxl2a9x4-OlesyaWhat do you think about Sofia?
+
+https://go.tinder.com/_PiYEV2a9y0-SofiaWhat do you think about Елизавета?
+
+https://go.tinder.com/AXDe2F2a9z0-%D0%95%D0%BB%D0%B8%D0%B7%D0%B0%D0%B2%D0%B5%D1%82%D0%B0What do you think about Алёна?
+
+https://go.tinder.com/qOPlM12a900-%D0%90%D0%BB%D1%91%D0%BD%D0%B0What do you think about юля?
+
+https://go.tinder.com/V8UUtV2a92E-%D1%8E%D0%BB%D1%8FWhat do you think about Anna?
+
+https://go.tinder.com/rkRtgV2a93I-AnnaWhat do you think about Анастасия?
+
+https://go.tinder.com/PKdzWl2a94c-%D0%90%D0%BD%D0%B0%D1%81%D1%82%D0%B0%D1%81%D0%B8%D1%8FWhat do you think about Диана?
+
+https://go.tinder.com/A5E5o12a95c-%D0%94%D0%B8%D0%B0%D0%BD%D0%B0
